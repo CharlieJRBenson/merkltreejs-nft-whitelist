065ae5fd608f2c6be13f3c13ea3f661bfb73a8c5
Charlie <chaza433@gmail.com>
2023-06-24 10:23:12 +0100

    update script to /n seperated not csv

--- test/generate-whitelist-csv-import.js
+++ test/generate-whitelist-csv-import.js
@@ -1,29 +1,31 @@
-const { ethers } = require('hardhat')
-const { MerkleTree } = require('merkletreejs')
-const { keccak256 } = ethers.utils
-const fs = require('fs')
+const { ethers } = require('hardhat');
+const { MerkleTree } = require('merkletreejs');
+const { keccak256 } = ethers.utils;
+const fs = require('fs');
 
 // Read the file and split the addresses into an array
-const accounts = fs.readFileSync('whitelisted-accounts.csv', 'utf8').split(',')
+const csvData = fs.readFileSync('whitelisted-accounts.csv', 'utf8');
+const accounts = csvData.trim().split('\n');
 
 const padBuffer = (addr) => {
-    return Buffer.from(addr.substr(2).padStart(32 * 2, 0), 'hex')
-}
+    return Buffer.from(addr.substr(2).padStart(32 * 2, '0'), 'hex');
+};
 
-const leaves = accounts.map(account => padBuffer(account))
-const tree = new MerkleTree(leaves, keccak256, { sort: true })
-const merkleRoot = tree.getHexRoot()
-console.log(merkleRoot)
+const leaves = accounts.map((account) => padBuffer(account));
+const tree = new MerkleTree(leaves, keccak256, { sort: true });
+const merkleRoot = tree.getHexRoot();
+console.log(merkleRoot);
 
-let csvData = ""
+let outputCsvData = '';
 
 // For each account, generate a merkle proof and add to CSV string
-for (const element of accounts) {
-    const account = element;
-    const merkleProof = tree.getHexProof(padBuffer(account))
-    const proofString = JSON.stringify(merkleProof).replace(/"/g, '""')
-    csvData += `${account},"""${proofString}"""\n`
+for (const account of accounts) {
+    const paddedAccount = padBuffer(account);
+    const merkleProof = tree.getHexProof(paddedAccount);
+
+    const proofString = JSON.stringify(merkleProof).replace(/"/g, '""');
+    outputCsvData += `${account},"""${proofString}"""\n`;
 }
 
 // Write to CSV file
-fs.writeFileSync('merkleProofs.csv', csvData)
+fs.writeFileSync('merkleProofs.csv', outputCsvData);
